af23d8d3c4d7816286d916acd16179ca7798598f
RichartM <20233tn230@utez.edu.mx>
2026-02-17 12:46:16 -0600

    Arquitectura final: Limpieza de archivos duplicados y submódulos

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './Home';
+
+
+const IndexPage: React.FC = () => {
+    return (
+        <React.Fragment>
+            <Home />
+        </React.Fragment>
+    );
+};
+
+export default IndexPage;
